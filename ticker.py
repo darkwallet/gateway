604aa242f0bccdd26375863e4176aa0c9529355f
Amir Taaki <genjix@riseup.net>
2014-03-25 21:40:55 +0000

    catch also urlerror when querying ticker.

--- ticker.py
+++ ticker.py
@@ -28,7 +28,7 @@ class Ticker(threading.Thread):
         url = "https://api.bitcoinaverage.com/ticker/global/all"
         try:
             f = urllib2.urlopen(url)
-        except urllib2.HTTPError:
+        except urllib2.HTTPError, urllib2.URLError:
             return
         return json.loads(f.read())
 
