fb65eb2b0013c226e39a93ef11065bd8264e6ae7
Pablo Martin (wdp) <caedes@sindominio.net>
2015-01-23 00:13:50 +0100

    Add eur price to status.

--- daemon/status.py
+++ daemon/status.py
@@ -9,7 +9,7 @@ class StatusHandler(tornado.web.RequestHandler):
         stats = {
             'brc': { 'peers': self.app.brc_handler._brc.last_nodes, 'issues': self.app.brc_handler._brc.issues },
             'radar': { 'peers': self.app.brc_handler._radar.radar_hosts, 'issues': self.app.brc_handler._radar.issues },
-            'ticker': {'issues': self.app.ticker_handler._ticker.issues},
+            'ticker': {'issues': self.app.ticker_handler._ticker.issues, 'price': self.app.ticker_handler._ticker.ticker.get('EUR', {}).get('24h_avg', 'error')},
             'p2p': {'peers': len(self.app.p2p._peers.keys())}
         }
         self.write(json.dumps(stats))
